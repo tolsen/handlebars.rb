0488158e58c9bd7fe5973b61680f29d538f9aaae
idris <idris.m@gmail.com>
2011-06-27 10:01:56 -0400

    Using rhino instead of V8 so that we can run in jRuby

--- lib/handlebars.rb
+++ lib/handlebars.rb
@@ -1,55 +1,61 @@
-require 'v8'
+require 'rhino'
 require 'multi_json'
 
 
 module Handlebars
-  
-  V8::Context.new do |cxt|
-    cxt.load(File.expand_path(File.join(File.dirname(__FILE__), '..','js','lib','handlebars.js')))
-    @context = cxt
-    @handlebars = cxt['Handlebars']
-  end
-  
-  class << self
-    attr_reader :context
-  end
-  
-  class CompiledTemplate
-    
-    def initialize(context, template)
-      @context, @template = context, template
-    end
-    
-    def call(data)
-      json = MultiJson.encode(data)
-      @context['template'] = @template
-      @context.eval "template(#{json})"
-    end
-    
-  end
-  
-  
-  def compile(*args)
-    Handlebars.module_eval do
-      CompiledTemplate.new(@context, @handlebars.compile(*args))
-    end
-  end
-  module_function :compile
-  
-  
-  def registerHelper(name, fn)
-    Handlebars.module_eval do
-      @handlebars.registerHelper(name, fn)
-    end
-  end
-  module_function :registerHelper
 
-  def registerPartial(name, template)
-    Handlebars.module_eval do
-      @handlebars.registerPartial(name, template)
-    end
-  end
-  module_function :registerPartial
-  
-  
+	Rhino::Context.open do |cxt|
+		cxt.load(File.expand_path(File.join(File.dirname(__FILE__), '..','js','lib','handlebars.js')))
+		@context = cxt
+		@handlebars = cxt['Handlebars']
+		cxt['alert'] = lambda { |msg, obj| nil }#puts msg; puts obj.inspect }
+	end
+
+	class << self
+		attr_reader :context
+	end
+
+	class CompiledTemplate
+
+		def initialize(context, template)
+			@context, @template = context, template
+		end
+
+		def call(data)
+			json = MultiJson.encode(data)
+			@context['template'] = @template
+			@context.eval "template(#{json})"
+		end
+
+	end
+
+
+	def compile(template, options=nil)
+		Handlebars.module_eval do
+			@context['template'] = template
+			@context['options'] = options
+
+			CompiledTemplate.new(@context, @context.eval('Handlebars.compile(template, options)'))
+		end
+	end
+	module_function :compile
+
+
+	def registerHelper(name, fn)
+		Handlebars.module_eval do
+			@context['name'] = name
+			@context['fn'] = fn
+			@context.eval('Handlebars.registerHelper(name, fn);')
+		end
+	end
+	module_function :registerHelper
+
+	def registerPartial(name, template)
+		Handlebars.module_eval do
+			@context['name'] = name
+			@context['template'] = template
+			@context.eval('Handlebars.registerPartial(name, template);')
+		end
+	end
+	module_function :registerPartial
 end
